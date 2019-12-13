4892f77457f4e8e989c2603496e1c2fd000ee03f
Jarl Gullberg <jarl.gullberg@gmail.com>
2019-12-13 11:19:16 +0100

    Add a set of recoloured Mona Lisa images.

--- Puzzle.Tests/Data/SampleImages.cs
+++ Puzzle.Tests/Data/SampleImages.cs
@@ -21,6 +21,7 @@
 //
 
 using System;
+using System.Collections.Generic;
 using System.Reflection;
 using System.Threading;
 using SixLabors.ImageSharp;
@@ -32,6 +33,83 @@ namespace Puzzle.Tests.Data
     /// </summary>
     public static class SampleImages
     {
+        /// <summary>
+        /// Gets a set of slightly edited Mona Lisa images.
+        /// </summary>
+        public static IEnumerable<object[]> SlightlyEditedImages
+        {
+            get
+            {
+                yield return new object[] { Lisa9.Value };
+                yield return new object[] { ChromaticMona.Value };
+            }
+        }
+
+        /// <summary>
+        /// Gets a set of significantly edited Mona Lisa images.
+        /// </summary>
+        public static IEnumerable<object[]> SignificantlyEditedImages
+        {
+            get
+            {
+                yield return new object[] { BritneySpearsMona.Value };
+                yield return new object[] { GangstaMona.Value };
+            }
+        }
+
+        /// <summary>
+        /// Gets a set of recoloured Mona Lisa images.
+        /// </summary>
+        public static IEnumerable<object[]> RecolouredImages
+        {
+            get
+            {
+                yield return new object[] { BlueMona.Value };
+                yield return new object[] { CyanMona.Value };
+                yield return new object[] { GreenMona.Value };
+                yield return new object[] { PinkMona.Value };
+                yield return new object[] { RedMona.Value };
+                yield return new object[] { YellowMona.Value };
+            }
+        }
+
+        /// <summary>
+        /// Gets a set of stylized Mona Lisa image copies.
+        /// </summary>
+        public static IEnumerable<object[]> StylizedCopies
+        {
+            get
+            {
+                yield return new object[] { HorrorMona.Value };
+                yield return new object[] { MonaLizzle.Value };
+                yield return new object[] { PointilistMona.Value };
+            }
+        }
+
+        /// <summary>
+        /// Gets a set of real-life Mona Lisa photographs, containing real people.
+        /// </summary>
+        public static IEnumerable<object[]> Photos
+        {
+            get
+            {
+                yield return new object[] { PhotoMona.Value };
+            }
+        }
+
+        /// <summary>
+        /// Gets a set of images that do not depict the Mona Lisa.
+        /// </summary>
+        public static IEnumerable<object[]> DifferentImages
+        {
+            get
+            {
+                yield return new object[] { SaintMatthew.Value };
+                yield return new object[] { SickBacchus.Value };
+                yield return new object[] { JudithBeheading.Value };
+            }
+        }
+
         /// <summary>
         /// Gets the canonical Mona Lisa image.
         /// </summary>
@@ -76,6 +154,36 @@ namespace Puzzle.Tests.Data
         /// Gets a real-life photograph Mona Lisa image.
         /// </summary>
         public static Lazy<Image> PhotoMona { get; } = GetLazyLoaderForImage("truth-lisa-photo.jpg");
+
+        /// <summary>
+        /// Gets a recoloured Mona Lisa image.
+        /// </summary>
+        public static Lazy<Image> BlueMona { get; } = GetLazyLoaderForImage("recoloured-mona-blue.jpg");
+
+        /// <summary>
+        /// Gets a recoloured Mona Lisa image.
+        /// </summary>
+        public static Lazy<Image> CyanMona { get; } = GetLazyLoaderForImage("recoloured-mona-cyan.jpg");
+
+        /// <summary>
+        /// Gets a recoloured Mona Lisa image.
+        /// </summary>
+        public static Lazy<Image> GreenMona { get; } = GetLazyLoaderForImage("recoloured-mona-green.jpg");
+
+        /// <summary>
+        /// Gets a recoloured Mona Lisa image.
+        /// </summary>
+        public static Lazy<Image> PinkMona { get; } = GetLazyLoaderForImage("recoloured-mona-pink.jpg");
+
+        /// <summary>
+        /// Gets a recoloured Mona Lisa image.
+        /// </summary>
+        public static Lazy<Image> RedMona { get; } = GetLazyLoaderForImage("recoloured-mona-red.jpg");
+
+        /// <summary>
+        /// Gets a recoloured Mona Lisa image.
+        /// </summary>
+        public static Lazy<Image> YellowMona { get; } = GetLazyLoaderForImage("recoloured-mona-yellow.jpg");
 
         /// <summary>
         /// Gets an original Caravaggio painting.
